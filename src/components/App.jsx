e52054b2d857186c1068514455597b61479a9247
PrysiazhnukSerhii <feremiks@gmail.com>
2022-07-05 22:37:13 +0200

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,11 +31,12 @@ export class App extends Component {
   submitContacts = (data, checkName) => {
     let findedСoincidence = true;
 
-    //  this.state.contacts.map(evn => {
-    //   if (evn.name === checkName) {
-    //     return (findedСoincidence = false);
-    //   }
-    // });
+    this.state.contacts.map(evn => {
+      if (evn.name === checkName) {
+        return (findedСoincidence = false);
+      }
+      ('');
+    });
 
     if (!findedСoincidence) {
       alert(`${checkName} is already in contacts`);
